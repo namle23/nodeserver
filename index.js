4b332cd6d1141781642ebb98810f72bb57fc5730
Nam Lê <quangnamle23@gmail.com>
2018-03-22 21:20:17 +0200

    Upload file local

--- index.js
+++ index.js
@@ -3,39 +3,48 @@ let multer = require('multer');
 let path = require('path');
 let app = express();
 
-const fileUpload = require('./fileUpload.js');
+//allow access to folder
+app.use('/uploads', express.static(__dirname + '/uploads'));
+app.use('/src', express.static(__dirname + '/src'));
 
-let storageMulter = multer.memoryStorage({
+let storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads/JSONconfigurator');
+  },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
   }
 });
 
-let storage_multiple = multer.memoryStorage({
+let storage_multiple = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads/JSONmodels');
+  },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
   }
 });
 
-let upload = multer({ storage: storageMulter });
+let upload = multer({ storage: storage });
 let uploads = multer({ storage: storage_multiple }).any('files', 20);
-
-app.use(express.static(__dirname + '/public'));
-
-app.post('/upload', upload.single('file'), fileUpload.uploadToGcs, function (req, res) {
-  const data = req.body;
-
-  // if (req.file && req.file.cloudStoragePublicUrl) {
-  //   data.fileUrl = req.file.cloudStoragePublicUrl;
-  // }
-});
-
-app.post('/uploads', fileUpload.uploadToGcs, function (req, res) {
-
-});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.post('/upload', upload.single('file'), (req, res) => {
+  return res.json('success');
+});
+
+app.post('/uploads', function (req, res) {
+  uploads(req, res, function (error) {
+    if (error) {
+      return res.end('Error uploading file!');
+    }
+    res.end('Files uploaded!');
+  })
 });
 
 app.listen(3000, () => console.log(`Listening on 3000`));
